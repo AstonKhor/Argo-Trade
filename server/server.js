ddb7b7c8eab8f3fdf673e03700a816ecc47489b5
Aston Khor <Astonkhor@chenas-MacBook-Pro.local>
2019-12-10 09:36:08 -0800

    improve routes

--- server/server.js
+++ server/server.js
@@ -5,5 +5,33 @@ const port = 3000;
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+// app.use('/', express.static('http://localhost:3001/Public'));
+
+app.get('/graph/getStocks', (req, res) => {
+  console.log('here')
+  let id = req.query.id;
+  let key = Object.keys(req.query);
+  res.redirect(`http://localhost:3001/graph/getStocks?${key[0]}=${id}`);
+})
+app.get('/tradeStock/getStocks', (req, res) => {
+  let id = req.query.id;
+  let key = Object.keys(req.query);
+  res.redirect(`http://localhost:3002/tradeStock/getStocks?${key[0]}=${id}`);
+})
+app.get('/about/getCompanies', (req, res) => {
+  let id = req.query.id;
+  let key = Object.keys(req.query);
+  res.redirect(`http://localhost:3001/about/getCompanies?${key[0]}=${id}`);
+})
+app.get('/analyst/getStocks', (req, res) => {
+  let id = req.query.id;
+  let key = Object.keys(req.query);
+  res.redirect(`http://localhost:3001/analyst/getStocks?${key[0]}=${id}`);
+})
+app.get('/earnings/getEarnings', (req, res) => {
+  let id = req.query.id;
+  let key = Object.keys(req.query);
+  res.redirect(`http://localhost:3001/earnings/getEarnings?${key[0]}=${id}`);
+})
 
 app.listen(port, () => { console.log(`server up and running. Now listing on port ${port}`)});
