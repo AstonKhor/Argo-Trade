efcefee8838b964dcb2326e6a03a9117761f124d
Aston Khor <Astonkhor@chenas-MacBook-Pro.local>
2019-12-09 16:53:20 -0800

    setup environment

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => { console.log(`server up and running. Now listing on port ${port}`)});
